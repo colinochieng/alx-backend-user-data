e1b1c26ec154971a779ea92f687f1d84fde32fae
colinochieng <colinokumu89@gmail.com>
2023-11-09 13:41:58 +0000

    User Authentications

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -26,7 +26,7 @@ def stats() -> str:
     return jsonify(stats)
 
 
-@app_views.route('/unauthorized')
+@app_views.route('/unauthorized', strict_slashes=False)
 def unauthorized_view() -> None:
     '''
     unauthorized route
@@ -34,7 +34,7 @@ def unauthorized_view() -> None:
     abort(401)
 
 
-@app_views.route('/forbidden')
+@app_views.route('/forbidden', strict_slashes=False)
 def forbidden_view() -> None:
     '''
     forbidden route
